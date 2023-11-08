d8018c42bb0337737373c98fadfaa85b4a53573f
wksaurey <70457200+wksaurey@users.noreply.github.com>
2023-11-07 20:22:15 -0700

    working on day 3, found a bug

--- day3.py
+++ day3.py
@@ -1,7 +1,7 @@
 from util import read_stripped_lines
 
 def main():
-    wirePaths = read_stripped_lines('input/day3.text')
+    wirePaths = read_stripped_lines('input/day3test.text')
     wirePaths = list(map(lambda element: element.split(','), wirePaths))
     wireHistories = []
 
@@ -9,35 +9,34 @@ def main():
         pathHistory = []
         lastPosition = (0, 0)
         for instruction in wirePath:
-            for index in range(int(instruction[1])):
-                lastPosition = completeInstruction(instruction, lastPosition)
+            for _ in range(int(instruction[1:])):
+                lastPosition = completeInstruction(instruction[0], lastPosition)
                 pathHistory.append(lastPosition)
         wireHistories.append(pathHistory)
 
-    print(len(wireHistories))
     # TODO: check each point in each wire history for all intersection points, then find the manhatten distance from those points (add x and y)
     intersections = []
     for firstPosition in wireHistories[0]:
-        for secondPosition in wireHistories[0]:
+        for secondPosition in wireHistories[1]:
             if firstPosition == secondPosition:
                 intersections.append(firstPosition)
 
-    print(len(intersections))
+    print(intersections)
     lengths = list(map(lambda position: abs(position[0]) + abs(position[1]), intersections))
-    print(min(lengths))
+    print(f'Min Dist: {min(lengths)}')
     # 165 too low
 
-def completeInstruction(instruction: str, lastPosition: tuple) -> tuple:
-    if instruction[0] == 'U':
+def completeInstruction(direction: str, lastPosition: tuple) -> tuple:
+    if direction == 'U':
         return (lastPosition[0], lastPosition[1]+1)
-    elif instruction[0] == 'D':
+    elif direction == 'D':
         return (lastPosition[0], lastPosition[1]-1)
-    elif instruction[0] == 'R':
-        return (lastPosition[0]+1, lastPosition[1])
-    elif instruction[0] == 'L':
+    elif direction == 'R':
+        return (lastPosition[0+1], lastPosition[1])
+    elif direction == 'L':
         return (lastPosition[0]-1, lastPosition[1])
     else:
-        print(f'Invalid path instruction {instruction[0]}')
+        print(f'Invalid path instruction {direction}')
 
 if __name__ == '__main__':
     main()
