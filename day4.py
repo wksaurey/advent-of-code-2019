b7fa04dc72519f62b032a82ba4908c8a4bedf2ef
Kolter Saurey <Kolter.Saurey@asirobots.com>
2023-11-09 09:59:49 -0700

    Completed day4 parts 1 and 2

---
+++ day4.py
@@ -0,0 +1,22 @@
+INPUTMIN = 240298
+INPUTMAX = 784956
+
+potentialPasswordCount = 0
+
+for password in range(INPUTMIN, INPUTMAX):
+    # check adjacent
+    hasAdjacent = False
+    password = str(password)
+    for digit in password:
+        if str(int(digit)*11) in password and str(int(digit)*111) not in password:
+            hasAdjacent = True
+    # check decrease
+    neverDecrease = True
+    for index in range(1, len(password)):
+        if int(password[index-1]) > int(password[index]):
+            neverDecrease = False
+
+    if(hasAdjacent and neverDecrease):
+        potentialPasswordCount+=1
+
+print(f'Count: {potentialPasswordCount}')
