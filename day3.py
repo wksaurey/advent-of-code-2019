909935951343d3068288d144c96fae052dfd64c7
Kolter Saurey <Kolter.Saurey@asirobots.com>
2023-11-09 09:26:47 -0700

    Finished day3 part 2. needs optimization to run faster

--- day3.py
+++ day3.py
@@ -7,7 +7,7 @@ def main():
 
     for wirePath in wirePaths:
         pathHistory = []
-        lastPosition = (0, 0)
+        lastPosition = Position(0, 0, 0)
         for instruction in wirePath:
             for _ in range(int(instruction[1:])):
                 lastPosition = completeInstruction(instruction[0], lastPosition)
@@ -18,26 +18,31 @@ def main():
     intersections = []
     for firstPosition in wireHistories[0]:
         for secondPosition in wireHistories[1]:
-            if firstPosition == secondPosition:
-                intersections.append(firstPosition)
+            if firstPosition.x == secondPosition.x and firstPosition.y == secondPosition.y:
+                intersections.append(Position(firstPosition.x, firstPosition.y, firstPosition.steps+secondPosition.steps))
 
-    print(intersections)
-    lengths = list(map(lambda position: abs(position[0]) + abs(position[1]), intersections))
+    # lengths = list(map(lambda position: abs(position[0]) + abs(position[1]), intersections))
+    lengths = list(map(lambda position: position.steps, intersections))
     print(f'Min Dist: {min(lengths)}')
     # 165 too low
 
 def completeInstruction(direction: str, lastPosition: tuple) -> tuple:
     if direction == 'U':
-        return (lastPosition[0], lastPosition[1]+1)
+        return Position(lastPosition.x, lastPosition.y+1, lastPosition.steps+1)
     elif direction == 'D':
-        return (lastPosition[0], lastPosition[1]-1)
+        return Position(lastPosition.x, lastPosition.y-1, lastPosition.steps+1)
     elif direction == 'R':
-        newPosition = (lastPosition[0]+1, lastPosition[1])
-        return newPosition
+        return Position(lastPosition.x+1, lastPosition.y, lastPosition.steps+1)
     elif direction == 'L':
-        return (lastPosition[0]-1, lastPosition[1])
+        return Position(lastPosition.x-1, lastPosition.y, lastPosition.steps+1)
     else:
         print(f'Invalid path instruction {direction}')
+
+class Position:
+    def __init__(self, x: int, y: int, steps: int) -> None:
+        self.x = x
+        self.y = y
+        self.steps = steps
 
 if __name__ == '__main__':
     main()
